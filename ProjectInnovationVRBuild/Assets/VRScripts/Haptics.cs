0daf28c69ea60e1ce4b08f350ea44d8d08fc9e8b
Ana-Maria Ilea <62722821+Ana-MariaIlea@users.noreply.github.com>
2020-12-02 13:43:17 +0100

    Added scripts, player prefab, generated input actions

---
+++ ProjectInnovationVRBuild/Assets/VRScripts/Haptics.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Valve.VR;
+
+public class Haptics : MonoBehaviour
+{
+    public SteamVR_Input_Sources source;
+    public SteamVR_Action_Vibration hapticAction;
+    bool hapticFlag = false;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (hapticFlag == true)
+        {
+            Pulse(0.5f, 100, 50, source);
+        }
+    }
+
+    private void Pulse(float duration, float frequency, float amplitude, SteamVR_Input_Sources source)
+    {
+        hapticAction.Execute(0, duration, frequency, amplitude, source);
+        Debug.Log("Pulse");
+    }
+
+    /*
+    private void OnTriggerEnter(Collider other)
+    {
+        Debug.Log("Trigger start");
+        hapticFlag = true;
+    }
+    private void OnTriggerExit(Collider other)
+    {
+        Debug.Log("Trigger end");
+        hapticFlag = false;
+    }*/
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.tag != "Player" && collision.gameObject.GetComponent<Haptics>() == null)
+        {
+            Debug.Log("Collision start");
+            hapticFlag = true;
+        }
+    }
+
+    private void OnCollisionExit(Collision collision)
+    {
+        //collision.gameObject.GetComponent<Haptics>();
+        if (collision.gameObject.tag != "Player"&&collision.gameObject.GetComponent<Haptics>()==null)
+        {
+            Debug.Log("Collision end");
+            hapticFlag = false;
+        }
+        
+    }
+}
---
+++ ProjectInnovationVRBuild/Assets/VRScripts/VRController.cs
@@ -0,0 +1,129 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Valve.VR;
+
+[RequireComponent(typeof(CharacterController))]
+public class VRController : MonoBehaviour
+{
+    public SteamVR_Action_Boolean movePress = null;
+   // public SteamVR_Action_Vector2 moveValueControllerLeft = null;
+   // public SteamVR_Action_Vector2 moveValueControllerRight = null;
+
+
+    [SerializeField]
+    private float MaxSpeed=4;
+    private float speed=0;
+
+    private CharacterController characterController = null;
+    public Transform CameraRig;
+    public Transform Head;
+
+    bool canMove=true;
+
+    public void EnableMovement()
+    {
+        canMove = true;
+    }
+
+    public void DisableMovement()
+    {
+        canMove = false;
+    }
+
+    private void Awake()
+    {
+        characterController = GetComponent<CharacterController>();
+    }
+
+    private void Start()
+    {
+       // CameraRig = SteamVR_Render.Top().origin;
+       // Head = SteamVR_Render.Top().head;
+    }
+
+    private void Update()
+    {
+        HandleHead();
+        HandleHeight();
+        if (canMove)
+        {
+            CalculateMovement();
+        }
+
+    }
+
+    private void HandleHead()
+    {
+        //Store current 
+        Vector3 oldPosition = CameraRig.position;
+        Quaternion oldRotation = CameraRig.rotation;
+
+        //Rotate
+        transform.eulerAngles = new Vector3(0, Head.rotation.eulerAngles.y, 0);
+
+        //Restore position
+        CameraRig.position = oldPosition;
+        CameraRig.rotation = oldRotation;
+    }
+
+    private void CalculateMovement()
+    {
+        // Figure out orientation
+        Vector3 orientationEuler = new Vector3(0, transform.eulerAngles.y, 0);
+        Quaternion orientation = Quaternion.Euler(orientationEuler);
+        Vector3 movement = Vector3.zero;
+
+        // If not moving
+        if (movePress.GetStateUp(SteamVR_Input_Sources.Any))
+        {
+            speed = 0;
+        }
+
+
+        // If buttonpressed
+
+        if (movePress.GetState(SteamVR_Input_Sources.Any))
+        {
+            speed = MaxSpeed;
+            movement = orientation * (speed * Vector3.forward) * Time.deltaTime;
+        }
+        //if (movePress.GetState(SteamVR_Input_Sources.RightHand))
+        //{
+        //    speed = 4;
+        //    movement = orientation * (speed * Vector3.forward) * Time.deltaTime;
+        //}
+
+        //if (movePress.GetState(SteamVR_Input_Sources.LeftHand))
+        //{
+        //    speed = -4;
+        //    movement = orientation * (speed * Vector3.forward) * Time.deltaTime;
+        //}
+
+        // Apply
+        characterController.Move(movement);
+    }
+
+    private void HandleHeight()
+    {
+        // Get head in local space
+        float headHeight = Mathf.Clamp(Head.localPosition.y, 1, 2);
+        characterController.height = headHeight;
+
+        // Cut in half
+        Vector3 newCenter = Vector3.zero;
+        newCenter.y = characterController.height / 2;
+        newCenter.y += characterController.skinWidth;
+
+        // Move capsule in local space
+        newCenter.x = Head.localPosition.x;
+        newCenter.z = Head.localPosition.z;
+
+        // Rotate
+        newCenter = Quaternion.Euler(0, -transform.eulerAngles.y, 0) * newCenter;
+
+        // Apply
+        characterController.center = newCenter;
+    }
+
+}
